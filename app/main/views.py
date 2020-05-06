45f5e6270afb62c5f09c3f9aba04e643ef97f159
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-06 07:33:02 +0300

    Created a profile page where one can see details about there account.Added a bio and profile pic path to the database for storing the information an user gives to the app and the path of the profile picture of the user. Made an update functionality where one can always update there profile at leisure. Made the functionalities for manouverng through the update profile and profile templates of a user

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import Pitch
 from flask_login import login_required
+from ..models import User
+from  ..import db
+from .forms import UpdateProfile
 
 # views
 @main.route('/')
@@ -20,4 +23,33 @@ def movie(category):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('categories.html',category = category)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user=User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    
+    form=UpdateProfile()
+    if form.validate_on_submit():
+        user.bio=form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
 
--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class User(UserMixin,db.Model):
     username=db.Column(db.String(255))
     email =db.Column(db.String(255),unique=True,index=True)
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     # pitch_id=db.Column(db.Integer,db.ForeignKey('pitch.id'))
     pass_secure = db.Column(db.String(255))
 
