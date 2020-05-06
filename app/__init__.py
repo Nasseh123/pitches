c8c3e2a3289fab8f81c42abc9f74c93a65483eac
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-06 07:56:30 +0300

    Enabled the user profile to be able to upload there image to there profile.COnfig the location of the photos to be stored and made the route responsible for posting the photos

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
+from werkzeug.utils import secure_filename
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager=LoginManager()
 login_manager.session_protection='strong'
 login_manager.login_view='auth.login'
+
+photos = UploadSet('photos',IMAGES)
 
 # Initializing Flask Extensions
 bootstrap=Bootstrap()
@@ -29,6 +32,9 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # Will add the views and forms
 
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://manasseh:beast@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'      #specifies the destination to where we want to store our Images.
 
 class ProdConfig(Config):
     '''
